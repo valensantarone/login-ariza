0908b5b41c2781eae1b20f40f1f41d2bc660c0ef
Gonzaaa80 <150632193+Gonzaaa80@users.noreply.github.com>
2024-06-26 19:28:21 -0300

    Update index.php
    arreglo

--- index.php
+++ index.php
@@ -48,8 +48,7 @@
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css"/>
     <link rel="stylesheet" href="styles.css" />
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js"></script>
-    <script src="gonza.js?1" defer></script>
-    <script src="JavaScriptL.js?34" defer></script>
+
    
     <title>Log in</title>
 </head> 
